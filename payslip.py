6fdd67144de952a0b2349ac15b776f1e0c53c31f
Thomas Ward <thomasward9898@gmail.com>
2020-12-16 20:32:05 +1100

    adding a payslip class to encapsulate data extracted from the payslip PDF

---
+++ payslip.py
@@ -0,0 +1,13 @@
+class payslip:
+
+    def __init__ (dateWeekEnding, grossAmount, netAmount, taxedAmount\
+    , superAmount, annualLeaveAmount, studentLoanAmount):
+         this.dateWeekEnding = dateWeekEnding
+         this.grossAmount = grossAmount
+         this.netAmount = netAmount
+         this.taxedAmount = taxedAmount
+         this.superAmount = superAmount
+         this.annualLeaveAmount = annualLeaveAmount
+         this.studentLoanAmount = studentLoanAmount
+
+    
---
+++ pdf_data_extractor.py
@@ -0,0 +1,4 @@
+"""
+ file resposble for extracting data from recently collected payslips
+ and constucting a payslip class then adding such classes to our postgres database
+"""
