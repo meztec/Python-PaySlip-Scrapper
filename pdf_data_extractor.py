72bbe33dd3b7cc59a4123a05452d7c3ff652e953
meztec <thomasward9898@gmail.com>
2020-12-17 20:37:31 +1100

    update

--- pdf_data_extractor.py
+++ pdf_data_extractor.py
@@ -20,10 +20,41 @@
 # print(pageObj.extractText()[grossEarningIdx:netEarningIdx])
 #print(pageObj.extractText())
 
-import tabula
-
+# import tabula
+#
 file = "payslips/W_E_13_Dec_2020.pdf"
+#
+# tables = tabula.read_pdf(file, pages = "all", multiple_tables = True)
+# # output just the first table in the PDF to a CSV
+# tabula.convert_into(file, "output.csv", output_format="csv", pages='all')
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import HTMLConverter
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfpage import PDFPage
+from cStringIO import StringIO
+import re
+import csv
 
-tables = tabula.read_pdf(file, pages = "all", multiple_tables = True)
-# output just the first table in the PDF to a CSV
-tabula.convert_into(file, "output.csv", output_format="csv", pages='all')
+
+def convert_pdf_to_html(path):
+    rsrcmgr = PDFResourceManager()
+    retstr = StringIO()
+    codec = 'utf-8'
+    laparams = LAParams()
+    device = HTMLConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
+    fp = file(path, 'rb')
+    interpreter = PDFPageInterpreter(rsrcmgr, device)
+    password = ""
+    maxpages = 0 #is for all
+    caching = True
+    pagenos=set()
+    for page in PDFPage.get_pages(fp, pagenos, maxpages=maxpages, password=password,caching=caching, check_extractable=True):
+        interpreter.process_page(page)
+    fp.close()
+    device.close()
+    str = retstr.getvalue()
+    retstr.close()
+    return str
+
+print(convert_pdf_to_html(file))
